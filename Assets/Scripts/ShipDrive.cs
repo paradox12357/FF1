5033f61d3226ab861b612fc5d0911bdbc740da6c
paradox12357 <eldon12357@gmail.com>
2024-02-13 20:30:17 -0500

    Splitscreen and Controller Support

--- Assets/Scripts/ShipDrive.cs
+++ Assets/Scripts/ShipDrive.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Windows;
+using UnityEngine.InputSystem;
 
 public class ShipDrive : MonoBehaviour
 {
@@ -9,11 +10,20 @@ public class ShipDrive : MonoBehaviour
     public float turnSpeed;
     public float gravMult;
     private Rigidbody rb;
+
+    private FF1 ff1;
+    private InputActionAsset inputActions;
+
+    private Vector2 moveInput;
     //public int onGround = 0;
     // Start is called before the first frame update
     void Start()
     {
+        ff1 = new FF1();
+        inputActions = GetComponent<PlayerInput>().actions;
         rb = GetComponent<Rigidbody>();
+        ff1.Player.Enable();
+        inputActions.Enable();
     }
 
     // Update is called once per frame
@@ -25,7 +35,10 @@ public class ShipDrive : MonoBehaviour
     }
     void Accelerate()
     {
-        if (UnityEngine.Input.GetKey(KeyCode.W))
+        //moveInput = ff1.Player.Move.ReadValue<Vector2>();
+        moveInput = inputActions["Move"].ReadValue<Vector2>();
+        print("Move Input: " + moveInput.x + " " + moveInput.y);
+        if (/*UnityEngine.Input.GetKey(KeyCode.W)*/ moveInput.y > 0)
         {
             rb.AddRelativeForce(new Vector3(0, 0, 100));
             print("Velocity: " + rb.velocity);
@@ -34,7 +47,7 @@ public class ShipDrive : MonoBehaviour
             //forceToAdd.y = 0;
             //rb.AddForce(forceToAdd * speed * 10);
         }
-        else if (UnityEngine.Input.GetKey(KeyCode.S))
+        else if (/*UnityEngine.Input.GetKey(KeyCode.S)*/moveInput.y < 0)
         {
             //rb.AddRelativeForce(new Vector3(Vector3.forward.x, 0, Vector3.forward.z) * -speed);
             rb.AddRelativeForce(new Vector3(0, 0, -100));
@@ -45,23 +58,24 @@ public class ShipDrive : MonoBehaviour
         Vector3 localVel = transform.InverseTransformDirection(rb.velocity);
         localVel.x = 0;
         rb.velocity = transform.TransformDirection(localVel);
-        if(UnityEngine.Input.GetKey(KeyCode.R))
-        {
-            rb.AddRelativeForce(new Vector3(1000, 0, 0));
-        }
-        if (UnityEngine.Input.GetKey(KeyCode.Q))
-        {
-            rb.AddRelativeForce(new Vector3(-1000, 0, 0));
-        }
+        //if(UnityEngine.Input.GetKey(KeyCode.R))
+        //{
+        //    rb.AddRelativeForce(new Vector3(1000, 0, 0));
+        //}
+        //if (UnityEngine.Input.GetKey(KeyCode.Q))
+        //{
+        //    rb.AddRelativeForce(new Vector3(-1000, 0, 0));
+        //}
         //rb.AddRelativeForce(new Vector3(0, -100, 0));
+        
     }
     void Turn()
     {
-        if (UnityEngine.Input.GetKey(KeyCode.D))
+        if (/*UnityEngine.Input.GetKey(KeyCode.D)*/ moveInput.x > 0)
         {
             rb.AddTorque(Vector3.up * turnSpeed);
         }
-        if (UnityEngine.Input.GetKey(KeyCode.A))
+        if (/*UnityEngine.Input.GetKey(KeyCode.A)*/ moveInput.x < 0)
         {
             rb.AddTorque(-Vector3.up * turnSpeed);
         }
