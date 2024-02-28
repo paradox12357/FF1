a2e22db3bbeab17aa6ebe703c68f3bd02a9d29cf
TheWOB <williamobrien2001@gmail.com>
2024-02-27 20:41:28 -0500

    Checkpoint UI implemented

--- Assets/Scripts/CheckpointCounter.cs
+++ Assets/Scripts/CheckpointCounter.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.Linq;
+using UnityEngine.UI;
+using TMPro;
 
 public class CheckpointCounter : MonoBehaviour
 {
@@ -10,6 +12,7 @@ public class CheckpointCounter : MonoBehaviour
     void Start()
     {
         checkpointNames = new String[maxCheckpoints];
+        checkpointText.text = "CP: 0/" + maxCheckpoints;
     }
 
     // Update is called once per frame
@@ -17,12 +20,13 @@ public class CheckpointCounter : MonoBehaviour
     {
 
     }
-    
+
     public int checkpointCount = 0;
     public bool hasFinished = false;
     public int maxCheckpoints = 3;
     public String[] checkpointNames;
-    
+    public TextMeshProUGUI checkpointText;
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.gameObject.name);
@@ -31,11 +35,13 @@ public class CheckpointCounter : MonoBehaviour
             checkpointNames[checkpointCount] = other.gameObject.name;
             checkpointCount++;
             Debug.Log("Checkpoint Count: " + checkpointCount);
+            checkpointText.text = "CP: " + checkpointCount + "/" + maxCheckpoints;
         }
         if (other.gameObject.tag == "Finish" && checkpointCount >= maxCheckpoints)
         {
             hasFinished = true;
             Debug.Log("You have reached the finish line!");
+            checkpointText.text = "GOAL!";
         }
     }
 }
