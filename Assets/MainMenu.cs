811196a809def77d6a51b888c43c2fedb751f3d9
btran220033 <loc22003@gmail.com>
2024-02-27 20:39:42 -0500

    Main Menu Stuff
    Temporary Main Menu UI for milestone

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void GoToOptions()
+    {
+        SceneManager.LoadScene("OptionMenu");
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
