eccb68e365b0d9dcfe15d8bcf4fcd67199211aed
ZylerR <78983388+ZylerR@users.noreply.github.com>
2024-04-23 14:45:58 -0400

    Homing Rockets Implemented
    Grey Rockets home in on AI Enemies when close. Blue/Red Rockets home in on Players when close.

--- Assets/Scripts/rocketWallHit.cs
+++ Assets/Scripts/rocketWallHit.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class rocketWallHit : MonoBehaviour
@@ -9,43 +10,137 @@ public class rocketWallHit : MonoBehaviour
     GameObject shotOwner;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        //print("Wall's Coords: x: " + other.transform.position.x + " Rocket's Coords: x: " + transform.position.x);
+        if (gameObject.tag == "rocketHoming")
         {
-            if(other.gameObject == shotOwner)
+            print("ROCKET COLLISION!!!");
+            if (other.gameObject.tag == "Player")
             {
-                //LMAO!!!
-                print("Self hit! (no dmg dealt)");
-            }
-            else
-            {
-                print("PLAYER HIT");
-                ShipDrive HealthModifier = other.gameObject.GetComponent<ShipDrive>();
-                if (HealthModifier != null)
+                if (other.gameObject == shotOwner)
                 {
-                    HealthModifier.HP -= 50;
+                    //LMAO!!!
+                    print("Self hit! (no dmg dealt)");
                 }
-                CheckpointCounter CPC = shotOwner.GetComponent<CheckpointCounter>();
-                CPC.scoreCount += 500;
-                CPC.updateCheckpoint();
+                else
+                {
+                    /*print("PLAYER HIT");
+                    ShipDrive HealthModifier = other.gameObject.GetComponent<ShipDrive>();
+                    if (HealthModifier != null)
+                    {
+                        HealthModifier.HP -= 50;
+                    }
+                    CheckpointCounter CPC = shotOwner.GetComponent<CheckpointCounter>();
+                    CPC.scoreCount += 500;
+                    CPC.updateCheckpoint();
+                    FindObjectOfType<SoundEffectPlayer>().Play("rocketExplode");
+                    Destroy(gameObject);*/
+                    transform.position = Vector3.MoveTowards(transform.position, other.transform.position, 1f);
+                    transform.up = other.transform.position - transform.position;
+                }
+
+            }
+        }
+        if (gameObject.tag == "rocket")
+        {
+            print("ROCKET COLLISION!!!");
+            if (other.gameObject.tag == "enemy")
+            {
+               
+                transform.position = Vector3.MoveTowards(transform.position, other.transform.position, 1f);
+                transform.up = other.transform.position - transform.position;
+            
+
+            }
+        }
+        if (Mathf.Abs(other.transform.position.x - transform.position.x) < 5 && Mathf.Abs(other.transform.position.y - transform.position.y) < 5 &&
+            Mathf.Abs(other.transform.position.z - transform.position.z) < 5)
+        {
+            print("NEAR!!!!");
+            if (other.gameObject.tag == "Player")
+            {
+                if (other.gameObject == shotOwner)
+                {
+                    //LMAO!!!
+                    print("Self hit! (no dmg dealt)");
+                }
+                else
+                {
+                    print("PLAYER HIT");
+                    ShipDrive HealthModifier = other.gameObject.GetComponent<ShipDrive>();
+                    if (HealthModifier != null)
+                    {
+                        HealthModifier.HP -= 50;
+                    }
+                    CheckpointCounter CPC = shotOwner.GetComponent<CheckpointCounter>();
+                    CPC.scoreCount += 500;
+                    CPC.updateCheckpoint();
+                    FindObjectOfType<SoundEffectPlayer>().Play("rocketExplode");
+                    Destroy(gameObject);
+                }
+
+            }
+            if (other.gameObject.name.Contains("Wall"))
+            {
+                //print("LMAOOOO");
                 FindObjectOfType<SoundEffectPlayer>().Play("rocketExplode");
                 Destroy(gameObject);
             }
+            if (other.gameObject.tag == "enemy")
+            {
+                print("Enemy Hit");
+                CheckpointCounter CPC = shotOwner.GetComponent<CheckpointCounter>();
+                CPC.scoreCount += 200;
+                CPC.updateCheckpoint();
+                FindObjectOfType<SoundEffectPlayer>().Play("rocketExplode");
+                Destroy(other.gameObject);
+                Destroy(gameObject);
+            }
+        }
+        
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (gameObject.tag == "rocketHoming")
+        {
+            print("ROCKET COLLISION!!!");
+            if (other.gameObject.tag == "Player")
+            {
+                if (other.gameObject == shotOwner)
+                {
+                    //LMAO!!!
+                    print("Self hit! (no dmg dealt)");
+                }
+                else
+                {
+                    /*print("PLAYER HIT");
+                    ShipDrive HealthModifier = other.gameObject.GetComponent<ShipDrive>();
+                    if (HealthModifier != null)
+                    {
+                        HealthModifier.HP -= 50;
+                    }
+                    CheckpointCounter CPC = shotOwner.GetComponent<CheckpointCounter>();
+                    CPC.scoreCount += 500;
+                    CPC.updateCheckpoint();
+                    FindObjectOfType<SoundEffectPlayer>().Play("rocketExplode");
+                    Destroy(gameObject);*/
+                    transform.position = Vector3.MoveTowards(transform.position, other.transform.position, 1f);
+                    transform.up = other.transform.position - transform.position;
+                }
+
+            }
+        }
+        if (gameObject.tag == "rocket")
+        {
+            print("ROCKET COLLISION!!!");
+            if (other.gameObject.tag == "enemy")
+            {
+               
+                transform.position = Vector3.MoveTowards(transform.position, other.transform.position, 1f);
+                transform.up = other.transform.position - transform.position;
             
-        }
-        if (other.gameObject.name.Contains("Wall"))
-        {
-            //print("LMAOOOO");
-            FindObjectOfType<SoundEffectPlayer>().Play("rocketExplode");
-            Destroy(gameObject);
-        }
-        if (other.gameObject.tag == "enemy")
-        {
-            print("Enemy Hit");
-            CheckpointCounter CPC = shotOwner.GetComponent<CheckpointCounter>();
-            CPC.scoreCount += 200;
-            CPC.updateCheckpoint();
-            FindObjectOfType<SoundEffectPlayer>().Play("rocketExplode");
-            Destroy(other.gameObject);
+
+            }
         }
     }
     public void setShotOwner(GameObject shotOwnerPassedIn)
